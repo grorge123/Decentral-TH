e8daa8c7c16fd732ba92984932477cdd23490fe8
Han-Wen Tsao <chodehirgd157842@gmail.com>
2022-02-06 17:52:53 +0800

    feat: add erc721

---
+++ ERC721.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Strings.sol";
+
+contract MyNFT is ERC721Enumerable, Ownable {
+    using Strings for uint256;
+
+    string baseURI;
+    string public notRevealedUri;
+    string public baseExtension = ".json";
+
+    mapping(uint256 => string) private _tokenURIs;
+
+    constructor(string memory initBaseURI)
+        ERC721("MyNFT", "MN")
+    {
+        setBaseURI(initBaseURI);
+    }
+
+    function mint(address _to,  string calldata _uri) external onlyOwner {
+        uint256 _tokenId = totalSupply();
+        _mint(_to, _tokenId);
+        _tokenURIs[_tokenId] = _uri;
+    }
+
+    function withdraw(address to) public onlyOwner {
+        uint256 balance = address(this).balance;
+        payable(to).transfer(balance);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        string memory _tokenURI = _tokenURIs[tokenId];
+        string memory base = _baseURI();
+
+        // If there is no base URI, return the token URI.
+        if (bytes(base).length == 0) {
+            return _tokenURI;
+        }
+        // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked).
+        if (bytes(_tokenURI).length > 0) {
+            return string(abi.encodePacked(base, _tokenURI));
+        }
+        // If there is a baseURI but no tokenURI, concatenate the tokenID to the baseURI.
+        return
+            string(abi.encodePacked(base, tokenId.toString(), baseExtension));
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
+    }
+}
