fee51572d2f8c4d65c25d16b7b510d36d009812b
grorge <g97116@yahoo.com>
2022-02-25 23:01:18 +0800

    feat: Add new web client

--- connect/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-const Parse = require('parse/node');
-var app = express();
-// var api = require("./NTapi");
-//setting middleware
-app.use(express.static("./")); //Serves resources from public folder
- 
-var server = app.listen(5050);
-console.log("Start listen port 5050!!!")
